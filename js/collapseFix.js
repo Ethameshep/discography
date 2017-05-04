c6b95107ae6eb2cfbe0e61b1f8e76499f7e982f0
ultimatezenzar <ultimatezenzar@gmail.com>
2017-05-04 10:05:10 -0500

    Script Deleted
    removed collapse fix.js

--- js/collapseFix.js
+++
@@ -1,7 +0,0 @@
-//auto uncollapses nav if the button dsiplay changes to none
-function navCheck() {
-    if(window.innerWidth > 1280) {
-        $(".collapse").collapse("show");
-        console.log("element uncollapsing");
-    }
-}
